7d7fe871f9291dd3ef98becd64a02862c8735c84
houskkam <kamila.hou@seznam.cz>
2023-09-18 12:10:22 -0300

    pluto notebook with information how to use it from class created

---
+++ notebook.jl
@@ -0,0 +1,46 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8d1c408c-c63d-4dc0-8c5c-7e061888f232
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f9aa115e-8d64-45da-a0d8-016b1637fb0b
+md"Pour écrire du markdown, la cellule doit contenir
+ Mon texte markdown.
+
+Pages contenant le nécessaire à propos du format markdown :
+- <https://www.jetbrains.com/help/hub/Markdown-Syntax.html#quick-notes-markdown-images>
+- Intégrant des équations <https://learninglab.gitlabpages.inria.fr/mooc-rr/mooc-rr-ressources/module1/ressources/introduction_to_markdown_fr.html#exposants-et-indices>
+
+Le code de chaque cellule peut se cacher ou se dévoiler en cliquant sur le symbole en forme d'oeil qui lui est associé.
+"
+
+# ╔═╡ 46f3fbc9-e6b1-4033-b216-1938e56c6c51
+md"
+# Test
+
+- An equation $x_{k+1} = x_k + \alpha_k g_k$
+
+- Some text : **en gras**, *en italique*
+"
+
+# ╔═╡ b5461c70-fc63-448a-8f41-c29e23106a95
+println("Ceci est une introduction à Pluto !")
+
+# Cell order:
+# ╟─7c12d04a-6c37-4baa-a264-ec4804c86e95
+# ╟─31787c7f-9668-4673-a4a2-9b8481eb2535
+# ╠═e41d694e-5400-11ee-3e29-853b9d5cb3b6
+
+# ╔═╡ Cell order:
+# ╠═8d1c408c-c63d-4dc0-8c5c-7e061888f232
+# ╠═f9aa115e-8d64-45da-a0d8-016b1637fb0b
+# ╠═46f3fbc9-e6b1-4033-b216-1938e56c6c51
+# ╠═b5461c70-fc63-448a-8f41-c29e23106a95
