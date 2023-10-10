0b72c35e7b32d3e5d399c02e2df75afcb4f29bdd
houskkam <kamila.hou@seznam.cz>
2023-10-09 23:28:57 -0300

    adding Pluto notebook about Phase1 and its pdf

--- notebook.jl
+++ notebook.jl
@@ -4,43 +4,58 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 8d1c408c-c63d-4dc0-8c5c-7e061888f232
-### A Pluto.jl notebook ###
-# v0.19.27
+# ╔═╡ 75572c3f-0fea-4b7a-aa67-6d97661f5da6
+#the repository can be found on the github link: https://github.com/houskkam/mth6412b-starter-code
+begin
+import Pkg
+Pkg.add("Plots")
+include("projet\\phase1\\node.jl")
+include("projet\\phase1\\edge.jl")
+include("projet\\phase1\\graph.jl")
+include("projet\\phase1\\read_stsp.jl")
+end
 
-using Markdown
-using InteractiveUtils
+# ╔═╡ 3334fbc2-a2bc-400e-9011-81fc018566ff
+#A file name (fn) is defined by obtaining the current working directory with pwd() and combining it with a relative path to a file named "bayg29.tsp."
+begin
+fn = pwd() * "\\instances\\stsp\\bayg29.tsp"
+#reads the data from the specified file (fn) and stores it in various variables such as header, almost_edges, and almost_nodes.
+header = read_header(fn)
+almost_edges = read_edges(header, fn)
+almost_nodes = read_nodes(header, fn)
+end
 
-# ╔═╡ f9aa115e-8d64-45da-a0d8-016b1637fb0b
-md"Pour écrire du markdown, la cellule doit contenir
- Mon texte markdown.
 
-Pages contenant le nécessaire à propos du format markdown :
-- <https://www.jetbrains.com/help/hub/Markdown-Syntax.html#quick-notes-markdown-images>
-- Intégrant des équations <https://learninglab.gitlabpages.inria.fr/mooc-rr/mooc-rr-ressources/module1/ressources/introduction_to_markdown_fr.html#exposants-et-indices>
+# ╔═╡ 6664eaff-46ae-4f26-a297-3eb2f2a74294
+# Constructing my_nodes of type Node from the given file 
+#starting with an empty array for nodes and filling it up with a for loop
+begin
+my_nodes = Vector{Node{Float64}}()
+for almost_node in almost_nodes
+    new_node = Node(string(almost_node[2][1]), almost_node[2][2])
+	#adding the new nodes
+    push!(my_nodes, new_node) 
+end
+end
 
-Le code de chaque cellule peut se cacher ou se dévoiler en cliquant sur le symbole en forme d'oeil qui lui est associé.
-"
+# ╔═╡ b91cfa11-627a-44d6-a18c-ae8a4220608e
+# Constructing my_edges of type Edge from the given file 
+#starting with an empty array for edges and filling it up with a for loop
+begin
+my_edges = Vector{Edge{Float64, Node{Float64}}}()
+for almost_edge in almost_edges
+    new_edge = Edge(my_nodes[almost_edge[1]], my_nodes[almost_edge[2]], almost_edge[3])
+	#adding the new edges
+    push!(my_edges, new_edge)
+end
+end
 
-# ╔═╡ 46f3fbc9-e6b1-4033-b216-1938e56c6c51
-md"
-# Test
-
-- An equation $x_{k+1} = x_k + \alpha_k g_k$
-
-- Some text : **en gras**, *en italique*
-"
-
-# ╔═╡ b5461c70-fc63-448a-8f41-c29e23106a95
-println("Ceci est une introduction à Pluto !")
-
-# Cell order:
-# ╟─7c12d04a-6c37-4baa-a264-ec4804c86e95
-# ╟─31787c7f-9668-4673-a4a2-9b8481eb2535
-# ╠═e41d694e-5400-11ee-3e29-853b9d5cb3b6
+# ╔═╡ ac1db0f2-ec4a-4834-80b1-6d0b4c0d91ea
+G = Graph("Ick", my_nodes, my_edges)
 
 # ╔═╡ Cell order:
-# ╠═8d1c408c-c63d-4dc0-8c5c-7e061888f232
-# ╠═f9aa115e-8d64-45da-a0d8-016b1637fb0b
-# ╠═46f3fbc9-e6b1-4033-b216-1938e56c6c51
-# ╠═b5461c70-fc63-448a-8f41-c29e23106a95
+# ╠═75572c3f-0fea-4b7a-aa67-6d97661f5da6
+# ╠═3334fbc2-a2bc-400e-9011-81fc018566ff
+# ╠═6664eaff-46ae-4f26-a297-3eb2f2a74294
+# ╠═b91cfa11-627a-44d6-a18c-ae8a4220608e
+# ╠═ac1db0f2-ec4a-4834-80b1-6d0b4c0d91ea
