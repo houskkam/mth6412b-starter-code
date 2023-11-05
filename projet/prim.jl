e790896544281a7a1df1c215746914025bb0f5fb
Victoria Vandenberghe <victoria.vandenberghe@polymtl.ca>
2023-11-05 16:13:19 +0100

    new prim with composanteconnexe

--- projet/prim.jl
+++ projet/prim.jl
@@ -1,5 +1,6 @@
 
 include("graph.jl")
+include("composante_connexe.jl")
 
 
 "
@@ -18,7 +19,7 @@ Telkens wanneer een knoop aan de boom wordt verbonden, moeten de attributen min_
 
 function prim_alg(graph::Graph{T, Z}, startpoint::Node{T}) where {T, Z}
     nodes_gr = nodes(graph)
-    minimum_spanning_tree = Vector{Edge{T, Z}}() 
+    minimum_spanning_tree = ComposanteConnexe{T, Z}(startpoint, [startpoint], Vector{Edge{T, Z}}())
 
     # un attribut min_weight -> Initialement, min_weight = -1 ; Initialement, le parent de chaque noeud est nothing
     for node in nodes_gr
@@ -29,8 +30,8 @@ function prim_alg(graph::Graph{T, Z}, startpoint::Node{T}) where {T, Z}
     #débute en un noeud source s(startpoint) choisi par l’utilisateur et l’attribut min_weight de s est 0 ;
     startpoint.min_weight = 0
 
-    # Initialiser un dictionnaire inTree pour garder la trace des noeuds inclus dans minimum spanning tree
-    inTree = Dict{Node{T}, Bool}()
+    # Initialisation de l'ensemble inTree pour suivre les nœuds inclus dans l'arbre de couverture minimale
+    inTree =  Set{Node{T}}([startpoint])
 
     #une file de priorité contient tous les noeuds qui n’ont pas encore été ajoutés à l’arbre et min_weight donne la priorité
     #chaque fois qu’un noeud est connecté à l’arbre, les attributs min_weight et parent de ceux qui n’ont pas encore été connectés doivent être mis à jour.
@@ -38,35 +39,35 @@ function prim_alg(graph::Graph{T, Z}, startpoint::Node{T}) where {T, Z}
     enqueue!(pq,startpoint.min_weight,startpoint)
 
     while !isempty(pq)
-        # The first vertex in the pair is the minimum key vertex
+        # Le premier nœud dans la paire est le nœud ayant le poids minimal
         w = dequeue!(pq)
 
-        # Different key values for the same vertex may exist in the priority queue
-        # The one with the least key value is always processed first, so ignore the rest
-        if inTree[w]
+        # Si le nœud est déjà inclus dans l'arbre, passez au suivant
+        if  w in inTree
             continue
         end
         
 
-        inTree[w] = true  # mark node as included in spanning tree
+        inTree = union(inTree, Set{Node{T}}([w]))  # Marquez le nœud comme inclus dans l'arbre
 
         # Iterate through all adjacent nodes of w
         for edge in get_edges_for_node(graph, w)
             u= ifelse(node1(edge) == w, node2(edge), node1(edge))  # Trouver l'autre nœud connecté par l'arête
             weight= poids(edge)
+
             # If v is not in min. spanning tree and the weight of (u, v) is smaller than the current key of v
-            if haskey(inTree,u) && !inTree[u] && weight< u.min_weight
-                u.min_weight= weight
+            if u ∉ inTree && weight < u.min_weight
+                u.min_weight = weight
                 u.parent = w
 
                 enqueue!(pq, u.minweight, u)
 
                 #add edge to minimum spanning tree
-                push!(minimum_spanning_tree, edge)
+                add_node_and_edge!(minimum_spanning_tree, u, edge)
             end
         end
     end
-    
+
     return minimum_spanning_tree
 
 end
