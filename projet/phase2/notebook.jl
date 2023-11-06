768a6b2d7d30eab96ee4bd3e56e10118943ff14a
houskkam <kamila.hou@seznam.cz>
2023-11-06 17:42:53 -0400

    continuing the rapport

--- projet/phase2/notebook.jl
+++ projet/phase2/notebook.jl
@@ -17,13 +17,113 @@ end
 
 # ╔═╡ 3334fbc2-a2bc-400e-9011-81fc018566ff
 # First exercice of Phase 2
-# We decided to represent connected component as a oriented graph with a root.
+# We decided to represent connected component as a oriented graph with a root. We created two new datatypes for that.
+# The first one is EdgeOriented, which implements AbstractEdge. It is very similar to normal Edge but it has a start and an end.
+# We also added a function that converts an Edge to EdgeOriented.
 begin
-fn = pwd() * "\\instances\\stsp\\bayg29.tsp"
-#reads the data from the specified file (fn) and stores it in various variables such as header, almost_edges, and almost_nodes.
-header = read_header(fn)
-almost_edges = read_edges(header, fn)
-almost_nodes = read_nodes(header, fn)
+    """Type abstrait dont d'autres types de edges orientés dériveront."""
+    abstract type AbstractEdgeOriented{Z, T <: AbstractNode} <: AbstractEdge{Z, T}  end
+
+    """Type représentant les edges orientés d'un graphe.
+
+    Exemple:
+            noeud1 = Node("James", "ahooj")
+            noeud2 = Node("Kirk", "guitar")
+            noeud3 = Node("Lars", "tdd")
+            edge1 = EdgeOriented(noeud1, noeud2, 5)
+            edge2 = EdgeOriented(noeud2, noeud3, 4)
+
+    """
+    mutable struct EdgeOriented{Z, T} <: AbstractEdgeOriented{Z, T}
+    debut::T
+    fin::T
+    poids::Z
+    end
+
+    # on présume que tous les edges dérivant d'AbstractEdge
+    # posséderont des champs `edge1`, `edge2` et `poids`.
+
+    """Renvoie le nom du premiere noeud d'un edge."""
+    debut(edge::AbstractEdgeOriented) = edge.debut
+
+    """Renvoie le nom du deuxieme noeud d'un edge."""
+    fin(edge::AbstractEdgeOriented) = edge.fin
+
+    ==(e1::AbstractEdgeOriented, e2::AbstractEdgeOriented) = (debut(e1) == debut(e2)) && (fin(e1) == fin(e2)) && (poids(e1) == poids(e2))
+
+    Base.convert(::Type{T}, e::Edge) where {T<:EdgeOriented} = EdgeOriented(node1(e), node2(e), poids(e))
+
+    """Affiche un edge."""
+    function show(edge::AbstractEdgeOriented)
+    println("Parent node ", debut(edge), "child node: ", fin(edge) ,", weight: ", poids(edge))
+    end
+end
+
+# Then we used it to create the ComposanteConnexe type, which is a connected component. It is a subtype of AbstractGraph, so all methods that are implemented for AbstractGraph function for ComposanteConnexe too.
+begin
+
+    """Type abstrait dont d'autres types de graphes dériveront."""
+    abstract type AbstractComposanteConnexe{T, Z} <: AbstractGraph{T, Z} end
+
+    """Type representant une composante connexe comme un ensemble de noeuds, .
+
+    Exemple :
+        noeud1 = Node("James", "ahooj")
+        noeud2 = Node("Kirk", "guitar")
+        noeud3 = Node("Lars", "tdd")
+        edge_oriented_1 = EdgeOriented(noeud1, noeud2, 5)
+        edge_oriented_2 = EdgeOriented(noeud2, noeud3, 4)
+        C = ComposanteConnexe(noeud1, [noeud1, noeud2, noeud3], [edge_oriented_1, edge_oriented_2])
+
+    Attention, tous les noeuds doivent avoir des données de même type.
+    """
+    mutable struct ComposanteConnexe{T, Z} <: AbstractComposanteConnexe{T, Z}
+    root::T
+    nodes::Vector{T}
+    edges::Vector{EdgeOriented{Z,T}}
+    end
+
+    """Ajoute un noeud et l'arret qui le relie au graphe."""
+    function add_node_and_edge!(composante::ComposanteConnexe{Node{T}, Z}, node::Node{T}, edge::EdgeOriented{Z, Node{T}}) where {T, Z}
+    push!(composante.nodes, node)
+    push!(composante.edges, edge)
+    composante
+    end
+
+
+    """Determines that connected components are equal if their contents equal."""
+    ==(c1::ComposanteConnexe, c2::ComposanteConnexe) = (nodes(c1) == nodes(c2)) && (edges(c1) == edges(c2))
+
+    """Takes a vector of connected components and merges them into one."""
+    function connect_into_one(composantes::Vector{ComposanteConnexe{T, Z}}, edge::EdgeOriented{Z, T}) where {T, Z}
+    new_component = composantes[1]
+    for node in nodes(composantes[2])
+        if !(node in nodes(new_component))
+        add_node!(new_component, node)
+        end
+    end
+    for edge in edges(composantes[2])
+        add_edge!(new_component, edge)
+    end
+    if(length(composantes) > 2)
+        print("have to connect more than 2 components")
+    end
+    add_edge!(new_component, edge)
+    new_component
+    end
+
+    """Affiche un graphe."""
+    function show(graph::ComposanteConnexe)
+    println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes")
+    for node in nodes(graph)
+        show(node)
+    end
+    println("and ", nb_edges(graph), "edges.")
+    for edge in edges(graph)
+        show(edge)
+    end
+    end
+
 end
 
 
