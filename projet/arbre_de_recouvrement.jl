b2857c6fa930d4260b657990826d54b4a5da5326
houskkam <kamila.hou@seznam.cz>
2023-11-06 10:31:43 -0400

    fixing some bugs, testing kruskal

--- projet/arbre_de_recouvrement.jl
+++ projet/arbre_de_recouvrement.jl
@@ -1,4 +1,5 @@
 include("graph.jl")
+include("composante_connexe.jl")
 
 function kruskal(graph::Graph{T, Z}) where {T, Z}
     sorted_edges = sort(graph.edges)
@@ -20,17 +21,19 @@ function kruskal(graph::Graph{T, Z}) where {T, Z}
             end
         end
         if should_add
-            edge = convert(EdgeOriented, edge)
+            edge = convert(EdgeOriented{Z,T}, edge)
+            print("#\n")
+            print(edge, length(add_edge_to))
             # create new one and add it to the list of existing components
             if length(add_edge_to) == 0
-                new_component = ComposanteConnexe(node1(edge), [node1(edge), node2(edge)], [edge])
+                new_component = ComposanteConnexe(debut(edge), [debut(edge), fin(edge)], [edge])
                 push!(connected_components, new_component)
             # add the edge and the node that connects it to the connected component
             elseif length(add_edge_to) == 1
                 if node1(edge) in nodes(add_edge_to[1])
-                    add_node_and_edge!(add_edge_to[1], node2(edge), edge)
+                    add_node_and_edge!(add_edge_to[1], fin(edge), edge)
                 else
-                    add_node_and_edge!(add_edge_to[1], node1(edge), edge)
+                    add_node_and_edge!(add_edge_to[1], debut(edge), edge)
                 end
             # I have to connect all components and the new edge from add_edge_to into one,
             # delete the old unconnected components               
@@ -40,6 +43,7 @@ function kruskal(graph::Graph{T, Z}) where {T, Z}
                     component_idx = findfirst(==(component), connected_components)
                     deleteat!(connected_components, component_idx)
                 end
+                add_edge!(connected_components, edge)
             end
             num_added_edges = num_added_edges + 1
         end
