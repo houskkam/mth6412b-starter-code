077bb4595f5f8df85c9de5ef37df5158e70b2772
houskkam <kamila.hou@seznam.cz>
2023-11-18 11:06:32 -0400

    creating Tree structure

---
+++ projet/tree.jl
@@ -0,0 +1,37 @@
+include("node.jl")
+
+abstract type AbstractTree{Node{T}} end
+
+
+mutable struct Tree{T} <: AbstractTree{Node{T}}
+    node::Node{T}
+    children::Vector{Tree{T}}
+    parent::Union{Tree{T}, Missing}
+end
+
+get_node(t::Tree) = t.node
+children(t::Tree) = t.children
+parent(t::Tree) = t.parent
+
+"""Creates a tree from a given g of type AbstractGraph. The argument parent is going to be the root."""
+function create_child!(g::AbstractGraph{Z, Node{T}}, parent::Tree{T}, already_added::Vector{Node{T}}) where T
+    root = get_node(parent)
+    
+    for n in nodes(g)
+        # if the node is not in the tree yet but there is
+        # an edge that connects it to the tree, add it
+        if isnothing( findfirst(x -> name(x)== name(n), already_added)) && !isnothing(get_edge(g, root, n))
+            new_item = Tree{T}(n, Vector{Tree{T}}(), parent)
+            push!(already_added, n)
+            push!(children(parent), new_item)
+        end
+    end
+
+    # repete the process for all subtrees
+    for c in children(parent)
+        create_child!(g, c, already_added)
+    end
+
+    parent
+end
+
