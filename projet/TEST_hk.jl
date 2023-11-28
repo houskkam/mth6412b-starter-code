c10e4e6e51953936de5a3d3761d3c3fb277f8ec0
Victoria Vandenberghe <victoria.vandenberghe@polymtl.ca>
2023-11-28 03:12:41 +0100

    new changes

---
+++ projet/TEST_hk.jl
@@ -0,0 +1,14 @@
+include("node.jl")
+include("graph.jl")
+using Test
+include("composante_connexe.jl")
+include("arbre_de_recouvrement.jl")
+include("hk_algorithm.jl")
+include("read_graph.jl")
+include("read_stsp.jl")
+
+G = get_graph_from_file(pwd() * "\\instances\\stsp\\bayg29.tsp")
+(lowest_sum, lowest_i) = (10000, 10000)
+
+
+held_karp(G,nodes(G)[1],60, 100.0)
--- projet/hk_algorithm.jl
+++ projet/hk_algorithm.jl
@@ -1,11 +1,14 @@
-include("composante_connexe.jl")
-include("graph.jl")
-include("prim.jl")
 include("node.jl")
+include("graph.jl")
+include("composante_connexe.jl")
+include("prim.jl")
+include("arbre_de_recouvrement.jl")
+
+
 "
 HK algorithm
 1. Let k=0, pi^0=0 and W=-inf. 
-2.Find a min 1-tree T-{pi}^k. 
+2. Find a min 1-tree T-{pi}^k. 
 3. Compute w(pi^k)=L(T_{pi}^k) -2*summation (pi_{i}). 
 4. Let W=max(W, w(pi^k)). 
 5. Let v^k=d^k-2 (with all of this vectors) where d^k contains the degrees of nodes in T_{pi}^k.  
@@ -22,9 +25,12 @@ l'algorithme contient plusieurs paramètres:
 "
 
 
-function min_one_tree(graph::Graph{Node{T}, Z}, start_point::Node{T}) where {T, Z}
+function min_one_tree(graph::Graph{Node{T}, Z}, root::Node{T}) where {T, Z}
     #get the adjacent edges of the root
     to_remove = get_oriented_edges(graph, root)
+
+    #get all the nodes
+    all_nodes= nodes(graph)
 
     # Creates a vector of all graph's node except the root
     nodes_base = filter(x -> x != root, all_nodes)
@@ -33,21 +39,20 @@ function min_one_tree(graph::Graph{Node{T}, Z}, start_point::Node{T}) where {T, 
     edges_base = filter(x -> !(x in to_remove), edges(graph))
     
     # Gets the MST tree and its corresponding connex component c for the subgraph graph[V\{root}]
-    components = kruskal(Graph("", nodes_base, edges_base))
+    component = kruskal(Graph("", nodes_base, edges_base))
+    #println(component)
+    length(component.nodes) != 1 && error("kruskal has more than 1 component")
+
     #kruskal only gives the composante_connexe back and we also want a tree to be given back
-    edges_tree = Vector{Edge{T}}()
-    for component in components
-        append!(edges_tree, edges(component))
-    end
-    tree_structure = Graph("MST Tree", nodes_base, edges_tree)
+    edges_tree = edges(component)
+
+    tree_structure = Graph("MST", nodes_base, edges_tree)
     
     # Get degrees of nodes in the components (assuming it's a directed graph)
     node_degrees = Dict(node => 0 for node in all_nodes)
-    for component in components
-        for edge in edges(component)
-            node_degrees[debut(edge)] += 1
-            node_degrees[fin(edge)] -= 1
-        end
+    for edge in edges(component)
+        node_degrees[debut(edge)] += 1
+        node_degrees[fin(edge)] += 1
     end
 
     # Filter nodes with degree 1
@@ -58,7 +63,6 @@ function min_one_tree(graph::Graph{Node{T}, Z}, start_point::Node{T}) where {T, 
     
     # Add the root and 2 cheapest arcs from the root to a leaf
     # Keep the component components updated
-    # ATTENTION: from now on, because the tree is now a 1-tree, the components does not contain the information for the edges touching root.
     # We are keeping the degree dictionary updated
     add_node!(tree_structure, root)
     for i in 1:2
@@ -69,14 +73,17 @@ function min_one_tree(graph::Graph{Node{T}, Z}, start_point::Node{T}) where {T, 
         node_degrees[ends(e)[2]] += 1
     end
     node_degrees[root] = 2
-    return tree_structure, components
+    return tree_structure, component
 end
 
 
 
-function compute_weight(graph::Graph{T,Z}, tour::Graph{T,Z}, pi::Dict{Node{T}, Float64}) where {T,Z}
+function compute_weight(graph::Graph{T, Z}, tour::Graph{T, Z}, pi::Vector{Float64}) where {T, Z}
     # Initialize total weight
     total_weight = 0.0
+
+    # Define a function to get the index of a node in the graph
+    node_index(graph, node) = findfirst(x -> x == node, nodes(graph))
 
     # Iterate over edges in the tour
     for edge in edges(tour)
@@ -84,46 +91,39 @@ function compute_weight(graph::Graph{T,Z}, tour::Graph{T,Z}, pi::Dict{Node{T}, F
         node1, node2 = ends(edge)
 
         # Update total weight with edge weight and Lagrangian multipliers (pi)
-        total_weight += weight(edge) + pi[node1] + pi[node2]
+        total_weight += weight(edge) + pi[node_index(graph, node1)] + pi[node_index(graph, node2)]
     end
 
     # Compute the weight of the tour using the 1-tree and pi vector
-    # Adjust the implementation based on your specific needs
 
     return total_weight
 end
 
 
-function update_degrees(graph::Graph{T,Z}, components, root::Node{T}, dict_pi::Dict{Node{T}, Float64}) where {T,Z}
-    # Create a dictionary to store the degrees of nodes
-    node_degrees = Dict{Node{T}, Int64}()
 
+
+function update_degrees(graph::Graph{T, Z}, components, root::Node{T}, pi::Vector{Float64}) where {T, Z}
     # Initialize degrees for all nodes
-    for node in nodes(graph)
-        node_degrees[node] = 0
-    end
+    node_degrees = zeros(Int64, nb_nodes(graph))
 
     # Update degrees based on the 1-tree
     for component in components
         for edge in edges(component)
             # Increment degrees of the nodes in the 1-tree
-            increase_degree!(node_degrees, ends(edge)[1])
-            increase_degree!(node_degrees, ends(edge)[2])
+            node_degrees[node_index(graph, ends(edge)[1])] += 1
+            node_degrees[node_index(graph, ends(edge)[2])] += 1
         end
     end
 
-    # If the root is not present in the dictionary, add it with a degree of 2
-    if !haskey(node_degrees, root)
-        node_degrees[root] = 2
-    end
+    # If the root is not present in the 1-tree, add it with a degree of 2
+    node_degrees[node_index(graph, root)] += 2
 
     # Update degrees based on the Lagrangian multipliers (pi)
-    for (node, degree) in node_degrees
-        node_degrees[node] += Int(round(dict_pi[node]))
-    end
+    node_degrees .+= round.(Int, pi)
 
     return node_degrees
 end
+
 
 
 # Implement your stopping criterion, e.g., based on degrees or other conditions
@@ -137,14 +137,38 @@ function stopping_criterion(v_k, max_iterations, max_time)
 end
 
 
-function held_karp(graph::Graph{Node{T}, Z}, root::Node{T}, max_iterations::Int, max_time::Float64, dict_pi::Dict{Node{T}, Float64}) where {T, Z}
-    n = size(graph, 1)
+
+function transform_matrix(graph::Graph{T, Z}, pi::Vector) where {T, Z}
+    # when d_{ij}= c_{ij}+ pi_i+ pi_j
+    transformed_graph = deepcopy(graph)
+
+    # Define a function to get the index of a node in the graph
+    node_index(graph, node) = findfirst(x -> x == node, nodes(graph))
+
+    # Loop over alle randen in de grafiek
+    for edge in edges(transformed_graph)
+        node1, node2 = ends(edge)
+
+        # Bereken de nieuwe gewichten op basis van de Lagrangiaanse multiplicatoren
+        new_weight = weight(edge) + pi[node_index(graph, node1)] + pi[node_index(graph, node2)]
+
+        # Werk de gewichten van de randen bij
+        set_weight!(transformed_graph, edge, new_weight)
+    end
+
+    return transformed_graph
+end
+
+function held_karp(graph::Graph{Node{T}, Z}, root::Node{T}, max_iterations::Int, max_time::Float64) where {T, Z}
+    #n = size(graph, 1)
+    n = length(nodes(graph))
     k = 0
-    pi = zeros(Int, n)
     W = -Inf
+    pi= zeros(Float64,n)
 
     starting_time = time()
 
+    t_k=1
     while true
         # Step 2: Find a min 1-tree T-{pi}^k
         tree_structure, components = min_one_tree(graph, root)
@@ -156,31 +180,33 @@ function held_karp(graph::Graph{Node{T}, Z}, root::Node{T}, max_iterations::Int,
         W = max(W, w_pk)
 
         # Step 5: Let v^k=d^k-2
-        node_degrees = update_degrees(graph, components, root, dict_pi)
+        node_degrees = update_degrees(graph, components, root, pi)
         v_k = node_degrees .- 2
 
         # Step 6: Stopping criterion
         elapsed_time = time() - starting_time
-        if stopping_criterion(v_k)
+        if stopping_criterion(v_k, max_iterations, max_time)
             break
         end
 
-        # Step 7: Choose a step size, t^k
-        t_k = 0.1  # Adjust the step size based on your needs
 
         # Step 8: Update pi
-        pi = pi + t_k * v_k
+        pi= pi + t_k * v_k
+        # Stap 9: Transform the weight of the edges based on the new lagrangian multiplicator
+        transformed_graph = transform_matrix(graph, pi)
 
-        # Step 9: Increment k and repeat
+        # Incrementeer k and repeat
         k += 1
+        # Step 7: Choose a step size, t^k
+        t_k = 1/k  # Adjust the step size based on your needs
     end
 
     return W
 end
 
-
 # Example usage:
 # Replace `graph` with your specific graph representation
+"
 graph = [
     0 10 15 20;
     10 0 35 25;
@@ -188,8 +214,8 @@ graph = [
     20 25 30 0
 ]
 
-max_iterations = 1000  # replace with your desired value
-max_time = 60.0  # replace with your desired value in seconds
-
+max_iterations = 100  
+max_time = 60.0  
 result = held_karp(graph, root, max_iterations, max_time)
-println("Optimal tour cost: ", result)
+println('Optimal tour cost: ', result)
+"
