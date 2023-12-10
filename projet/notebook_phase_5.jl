b35b34a54b33e340b5814c50dbf28a40180318aa
Victoria Vandenberghe <victoria.vandenberghe@polymtl.ca>
2023-12-10 17:37:52 +0100

    notebook

---
+++ projet/notebook_phase_5.jl
@@ -0,0 +1,18 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 75572c3f-0fea-4b7a-aa67-6d97661f5da6
+md"""
+# Rapport Projet 5
+## MTH6412B
+"""
+
+# ╔═╡ 630f16ad-1f55-42df-a771-e1f5e90b2898
+
+
+# ╔═╡ Cell order:
+# ╠═75572c3f-0fea-4b7a-aa67-6d97661f5da6
+# ╠═630f16ad-1f55-42df-a771-e1f5e90b2898
